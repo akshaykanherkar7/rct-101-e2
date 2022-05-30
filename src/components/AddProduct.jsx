d344128762de24b893d1fac97fa5267ef798c1d8
akshaykanherkar7 <akshaykanherkar7@gmail.com>
2022-05-30 16:51:57 +0530

    Eva C2

--- src/components/AddProduct.jsx
+++ src/components/AddProduct.jsx
@@ -1,33 +1,143 @@
+import {
+  Button,
+  Input,
+  Modal,
+  ModalBody,
+  Radio,
+  RadioGroup,
+  Select,
+  useDisclosure,
+} from "@chakra-ui/react";
 import React from "react";
+import axios from "axios";
+import { useEffect } from "react";
 
-const AddProduct = () => {
+const AddProduct = ({
+  setData,
+  setForm,
+  data,
+  form,
+  page,
+  limit,
+  setTotalcount,
+  totalcount,
+}) => {
   // TODO: Remove below const and instead import them from chakra
-  const Button = () => <div />;
-  const Modal = () => <div />;
-  const ModalBody = () => <div />;
-  const Input = () => <div />;
-  const Select = () => <div />;
-  const RadioGroup = () => <div />;
-  const Radio = () => <div />;
+  // Button = () => <div />;
+  // Modal = () => <div />;
+  // ModalBody = () => <div />;
+  // Input = () => <div />;
+  // Select = () => <div />;
+  // RadioGroup = () => <div />;
+  // Radio = () => <div />;
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const handleOnchange = (e) => {
+    let { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post("http://localhost:8080/products", form).then((res) => {
+      setData([...data, res]);
+    });
+  };
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/products?_page=${page}&_limit=${limit}`)
+      .then((res) => {
+        setData(res.data);
+        setTotalcount(Number(res.headers["x-total-count"]));
+      });
+  }, [setData, page, limit]);
 
   return (
     <>
-      <Button my={4} data-cy="add-product-button"></Button>
-      <Modal>
+      <Button my={4} data-cy="add-product-button" onClick={onOpen}>
+        Add New Product
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
         <ModalBody pb={6}>
-          <Input data-cy="add-product-title" />
-          <Select data-cy="add-product-category">
-            <option data-cy="add-product-category-shirt"></option>
-            <option data-cy="add-product-category-pant"></option>
-            <option data-cy="add-product-category-jeans"></option>
+          <label>Title</label>
+          <Input
+            data-cy="add-product-title"
+            name="title"
+            value={form.title}
+            onChange={handleOnchange}
+          />
+          <br />
+          <br />
+          <label>Category</label>
+          <Select
+            data-cy="add-product-category"
+            name="category"
+            value={form.category}
+            onChange={handleOnchange}
+          >
+            <option data-cy="add-product-category-shirt" value="shirt">
+              Shirt
+            </option>
+            <option data-cy="add-product-category-pant" value="pant">
+              Pant
+            </option>
+            <option data-cy="add-product-category-jeans" value="jeans">
+              jeans
+            </option>
           </Select>
-          <RadioGroup data-cy="add-product-gender">
-            <Radio data-cy="add-product-gender-male"></Radio>
-            <Radio data-cy="add-product-gender-female"></Radio>
-            <Radio data-cy="add-product-gender-unisex"></Radio>
+          <br />
+          <label>Gender</label>
+          <RadioGroup>
+            <Radio
+              data-cy="add-product-gender-male"
+              onChange={handleOnchange}
+              name="gender"
+              value="male"
+            >
+              Male
+            </Radio>
+            <Radio
+              data-cy="add-product-gender-female"
+              onChange={handleOnchange}
+              name="gender"
+              value="female"
+            >
+              Female
+            </Radio>
+            <Radio
+              data-cy="add-product-gender-unisex"
+              onChange={handleOnchange}
+              name="gender"
+              value="unisex"
+            >
+              Unisex
+            </Radio>
           </RadioGroup>
-          <Input data-cy="add-product-price" />
-          <Button data-cy="add-product-submit-button"></Button>
+          <br />
+
+          <label>Image URL</label>
+          <input
+            style={{ border: "1px solid lightgray" }}
+            type="url"
+            name="imageSrc"
+            value={form.imageSrc}
+            onChange={handleOnchange}
+          />
+          <br />
+          <br />
+          <label>Price</label>
+          <Input
+            data-cy="add-product-price"
+            name="price"
+            value={form.price}
+            onChange={handleOnchange}
+          />
+          <br />
+          <br />
+          <Button data-cy="add-product-submit-button" onClick={handleSubmit}>
+            Create
+          </Button>
         </ModalBody>
       </Modal>
     </>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,15 +1,36 @@
 import React from "react";
+import AddProduct from "./AddProduct";
+import Product from "./Product";
+import Pagination from "./Pagination";
+import { Flex, Grid } from "@chakra-ui/react";
+import { useState } from "react";
 
 const Products = () => {
   // TODO: Remove below const and instead import them from chakra
-  const Flex = () => <div />;
-  const Grid = () => <div />;
+  // Flex = () => <div />;
+  // Grid = () => <div />;
+  const [form, setForm] = useState({});
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [limit, setLimit] = useState(3);
+  const [totalcount, setTotalcount] = useState();
 
   return (
     <Flex>
-      {/*  AddProduct */}
-      <Grid>{/* List of Products */}</Grid>
-      {/* Pagination */}
+      <AddProduct
+        setForm={setForm}
+        form={form}
+        setData={setData}
+        data={data}
+        page={page}
+        limit={limit}
+        totalcount={totalcount}
+        setTotalcount={setTotalcount}
+      ></AddProduct>
+      <Grid>
+        <Product data={data}></Product>
+      </Grid>
+      <Pagination totalcount={totalcount} page={page} setPage={setPage} limit={limit} setLimit={setLimit}></Pagination>
     </Flex>
   );
 };
